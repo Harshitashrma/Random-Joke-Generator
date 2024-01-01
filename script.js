a02e87063caf3e448406b4f773a6c74b46076a49
Harshita Sharma <155089213+Harshitashrma@users.noreply.github.com>
2024-01-01 23:17:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const jokeContainer = document.getElementById("joke");
+const btn = document.getElementById("btn");
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+const getJoke = async () => {
+    jokeContainer.classList.remove("fade");
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        jokeContainer.textContent = `${data.joke}`;
+        jokeContainer.classList.add("fade");
+    } catch (error) {
+        console.error('Error fetching joke:', error);
+    }
+};
+
+btn.addEventListener("click",getJoke);
+getJoke();
